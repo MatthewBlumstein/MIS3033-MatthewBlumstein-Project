d0c52bee0b623887b84f0ea4e7e377ce5aad8163
Matt Blumstein <MattBlumstein@ou.edu>
2020-03-01 15:57:33 -0600

    Game Screen

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            imgCard1.Source = new BitmapImage(new Uri("C:\\Users\\blum7476\\Downloads\\cards_jpeg\\JPEG\\AH.jpg"));
+            imgCard2.Source = new BitmapImage(new Uri("C:\\Users\\blum7476\\Downloads\\cards_jpeg\\JPEG\\KH.jpg"));
+            imgCard3.Source = new BitmapImage(new Uri("C:\\Users\\blum7476\\Downloads\\cards_jpeg\\JPEG\\QH.jpg"));
+            imgCard4.Source = new BitmapImage(new Uri("C:\\Users\\blum7476\\Downloads\\cards_jpeg\\JPEG\\JH.jpg"));
+            imgCard5.Source = new BitmapImage(new Uri("C:\\Users\\blum7476\\Downloads\\cards_jpeg\\JPEG\\10H.jpg"));
+        }
+    }
+}
